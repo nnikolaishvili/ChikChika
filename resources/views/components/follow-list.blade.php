8492c799ca03306b1716f282100547b3e4f791d3
Nini Nikolaishvili <45388236+nnikolaishvili@users.noreply.github.com>
2022-06-25 02:50:51 +0400

    Added images and links to users to follow

--- resources/views/components/follow-list.blade.php
+++ resources/views/components/follow-list.blade.php
@@ -12,21 +12,23 @@
     @forelse($usersToFollow as $userToFollow)
         <div class="flex flex-shrink-0">
             <div class="flex-1 ">
-                <div class="flex items-center w-48">
-                    <div>
-                        <img class="inline-block h-10 w-auto rounded-full ml-4 mt-2"
-                             src="{{asset('images/default_profile_400x400.png')}}"
-                             alt=""/>
+                <a href="{{ route('profile', $userToFollow->username) }}" class="hover:font-bold">
+                    <div class="flex items-center w-48">
+                        <div>
+                            <img class="inline-block h-10 w-10 rounded-full ml-4 mt-2"
+                                 src="{{ $userToFollow->imageUrl() }}"
+                                 alt=""/>
+                        </div>
+                        <div class="ml-3 mt-3">
+                            <p class="text-base leading-6 text-white text-xs">
+                                {{ $userToFollow->name }}
+                            </p>
+                            <p class="text-xs leading-5 text-gray-400 group-hover:text-gray-300 transition ease-in-out duration-150">
+                                {{ "@" . $userToFollow->username }}
+                            </p>
+                        </div>
                     </div>
-                    <div class="ml-3 mt-3">
-                        <p class="text-base leading-6 text-white text-xs">
-                            {{ $userToFollow->name }}
-                        </p>
-                        <p class="text-xs leading-5 text-gray-400 group-hover:text-gray-300 transition ease-in-out duration-150">
-                            {{ "@" . $userToFollow->username }}
-                        </p>
-                    </div>
-                </div>
+                </a>
             </div>
             <div class="flex-1 px-4 py-2 m-2">
                 <form action="{{ route('user.follow', $userToFollow->id) }}" method="post"
